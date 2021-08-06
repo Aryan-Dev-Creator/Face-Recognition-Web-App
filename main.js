e4c8f0b488e59a5eaffc57d92a052bb1251340f9
Aryan-Dev-Creator <81810258+Aryan-Dev-Creator@users.noreply.github.com>
2021-08-06 17:00:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,3 +19,25 @@ function Take_Snapshot()
 console.log('ml5 version:', ml5.version);
 
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/NcoXEF18X/model.json', modelLoaded);
+
+function modelLoaded()
+{
+    console.log('Model Loaded!');
+}
+
+function Check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error){
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+} 
